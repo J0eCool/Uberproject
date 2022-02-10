517a6e1c1ea6b7ebf0f1d30e9644e2cf171b1ef3
J0eCool <count.j0ecool@gmail.com>
2022-02-10 01:47:43 -0500

    Start of a TypeScript project

---
+++ ts-graphics/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import fs from 'fs';
+const app = express();
+
+const PORT = 3137;
+
+app.get('*', (req, res) => {
+    console.log(`GET for ${req.path}`);
+    const dir = 'static';
+    var filename = `${dir}${req.path}`;
+    if (req.path == '/') {
+        filename = `${dir}/shadertoy.html`;
+    }
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            console.error(`Couldn't load file ${filename}`, err);
+            const code = 404; // Internal Server Error
+            res.statusCode = code;
+            res.send(`Error ${code}`)
+            return;
+        }
+        res.send(data.toString('utf8'));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Express listening at http://localhost:${PORT}`);
+});
