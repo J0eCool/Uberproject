0de7fc8499db8fa496a5edd08ee0ed0233a7d9f9
J0eCool <count.j0ecool@gmail.com>
2022-02-11 22:13:37 -0500

    src/static -> src/public, extract path resolver, add index.html

--- ts-graphics/src/app.ts
+++ ts-graphics/src/app.ts
@@ -4,20 +4,30 @@ const app = express();
 
 const PORT = 3137;
 
+/** Get the filename for the resource requested by a user */
+function getFilename(requested: string): string {
+    const dir = 'static';
+    var ret = `${dir}${requested}`;
+    if (requested == '/') {
+        ret = `${dir}/index.html`;
+    }
+    
+    // check dist/static for compiled client-side .ts
+    const tsDistDir = 'dist/public';
+    const altPath = `${tsDistDir}${requested}`;
+    if (fs.existsSync(altPath)) {
+        ret = altPath;
+    }
+
+    return ret;
+}
+
 app.get('*', (req, res) => {
     console.log(`GET for ${req.path}`);
-    const dir = 'static';
-    var filename = `${dir}${req.path}`;
-    if (req.path == '/') {
-        filename = `${dir}/shadertoy.html`;
-    }
-    if (fs.existsSync(`dist/${filename}`)) {
-        // check dist/static for compiled client-side .ts
-        filename = `dist/${filename}`;
-    }
-    fs.readFile(filename, (err, data) => {
+    const path = getFilename(req.path);
+    fs.readFile(path, (err, data) => {
         if (err) {
-            console.error(`Couldn't load file ${filename}`, err);
+            console.error(`Couldn't load file ${path}`, err);
             const code = 404; // Internal Server Error
             res.statusCode = code;
             res.send(`Error ${code}`)
