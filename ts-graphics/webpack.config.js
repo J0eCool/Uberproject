7a5d1c6c37b8cac815f729b74fa40f1397f5dede
J0eCool <count.j0ecool@gmail.com>
2022-02-13 00:16:05 -0500

    watch all three files I want build hyoigh

--- ts-graphics/webpack.config.js
+++ ts-graphics/webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/public/notes.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  mode: 'development',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'public/notes.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: {
+        notes: './src/public/notes.tsx',
+        shadertoy: './src/public/shadertoy.ts',
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'public/[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
